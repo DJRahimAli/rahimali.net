71c85774c1f105b533eec334f914483d53d38b97
Rahim Ali <11995519+DJRahimAli@users.noreply.github.com>
2025-12-16 19:02:28 +0000

    separate account links into own content box and rename relatedPages and relatedPages-content to box and content-box

--- resources/scripts/includes/visitorcounter.js
+++ resources/scripts/includes/visitorcounter.js
@@ -1,6 +1,6 @@
-document.write('<div class="relatedPages">\
+document.write('<div class="box">\
 	<h2 class="prevent-select">Total page views:</h2>\
-	<div class="relatedPages-content">\
+	<div class="content-box">\
 		<p id="stats"></p>\
 	</div>\
 </div>');
