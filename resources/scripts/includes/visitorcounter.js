709aac259b106cc544a85892ed9aad6afdaa9e0b
Rahim Ali <11995519+DJRahimAli@users.noreply.github.com>
2025-12-17 10:37:32 +0000

    made body use flexbox and set visitor counter to bottom of screen

--- resources/scripts/includes/visitorcounter.js
+++ resources/scripts/includes/visitorcounter.js
@@ -1,4 +1,4 @@
-document.write('<div class="box">\
+document.write('<div class="box" style="margin-top: 0px;">\
 	<h2 class="prevent-select">Total page views:</h2>\
 	<div class="content-box">\
 		<p id="stats"></p>\
