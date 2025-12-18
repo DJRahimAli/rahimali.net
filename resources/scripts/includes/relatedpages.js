115f7319eb2e3d37d3eefb849f6827a0475fba1a
Rahim Ali <11995519+DJRahimAli@users.noreply.github.com>
2025-12-18 22:50:42 +0000

    fix comma in relatedpages being clickable

--- resources/scripts/includes/relatedpages.js
+++ resources/scripts/includes/relatedpages.js
@@ -4,10 +4,10 @@ var htmlString = '<a href="' + relatedpages[0][0] + '">' + relatedpages[0][1] + 
 
 // add more relatedpages to the htmlString if there are more than one in the array (var i = 1;)
 for (var i = 1; i < relatedpagesLength; i++) {
-	htmlString += '<a>, </a><a href="' + relatedpages[i][0] + '">' + relatedpages[i][1] + '</a>'
+	htmlString += ', <a href="' + relatedpages[i][0] + '">' + relatedpages[i][1] + '</a>'
 }
 
 document.write('<div class="prevent-select box" style="margin-top: 0px;">\
 	<h2>Related pages:</h2>\
-	<div class="content-box">' + htmlString + '</div>\
+	<div class="content-box"><p style="margin: 0px;">' + htmlString + '</p></div>\
 </div>');
