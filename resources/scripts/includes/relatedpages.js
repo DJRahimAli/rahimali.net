b4bbc43862aba74a301eb0ebee30d2c014544552
Rahim Ali <11995519+DJRahimAli@users.noreply.github.com>
2026-01-02 07:16:12 +0000

    remove margin-top style from relatedpages

--- resources/scripts/includes/relatedpages.js
+++ resources/scripts/includes/relatedpages.js
@@ -7,7 +7,7 @@ for (var i = 1; i < relatedpagesLength; i++) {
 	htmlString += ', <a href="' + relatedpages[i][0] + '">' + relatedpages[i][1] + '</a>'
 }
 
-document.write('<div class="prevent-select box" style="margin-top: 0px;">\
+document.write('<div class="prevent-select box">\
 	<h2>Related pages:</h2>\
 	<div class="content-box"><p style="margin: 0px;">' + htmlString + '</p></div>\
 </div>');
