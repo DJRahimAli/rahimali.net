e5973ed79920181c11fecbe96328f3d0627600d9
Rahim Ali <11995519+DJRahimAli@users.noreply.github.com>
2026-03-04 02:20:34 +0000

    add help.js
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
    https://stackoverflow.com/questions/68928856/is-it-safe-to-check-for-keycodes-to-only-allow-certain-letters-on-keyboard-event
    https://www.w3schools.com/jsref/jsref_tolowercase.asp
    https://www.w3schools.com/jsref/prop_style_position.asp
    https://stackoverflow.com/questions/3616572/how-to-position-a-div-in-bottom-right-corner-of-a-browser
    https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/filter-function/drop-shadow
    https://stackoverflow.com/questions/287750/how-do-i-pre-cache-images-for-quick-viewing-with-javascript

---
+++ resources/scripts/help.js
@@ -0,0 +1,67 @@
+let helpString = "help";
+let helpStringCurrent = "";
+let helpStringLength = helpString.length;
+
+let helpkeyPressed = false;
+let helped = false;
+
+//preload image
+tempImg = new Image();
+tempImg.src="https://static.wikia.nocookie.net/thecreaturecases/images/c/c0/Kit_Casey_Best_Picture.png/revision/latest?cb=20230806011008";
+
+function createHelper() {
+	let helperElement = document.createElement('div');
+	helperElement.setAttribute("id", "helper");
+	//helperElement.setAttribute("class", "slideInRight");
+	helperElement.setAttribute("class", "prevent-select");
+	helperElement.setAttribute("style", "position: fixed; right: 0px; bottom: 0px; filter: drop-shadow(4px 4px 2px rgba(0,0,0,0.2)); animation: 0.6s ease-out forwards helperslideInRight, 0.6s ease-out 4s reverse forwards helperslideInRight;");
+	//helperElement.style.position = "absolute";
+	document.body.appendChild(helperElement);
+	includeHelper();
+}
+
+function includeHelper() {
+	document.getElementById("helper").innerHTML = '\
+	\
+<div class="tooltip" style="max-width: 240px; margin-right: 16px; margin-bottom: 2px;">\
+	<p style="margin: 0px;">Why don&apos;t you click, drag and use keyboard arrows?</p>\
+</div>\
+<img style="display: flex; width: 248px; height: auto; float: right; image-rendering: auto;" src="https://static.wikia.nocookie.net/thecreaturecases/images/c/c0/Kit_Casey_Best_Picture.png/revision/latest?cb=20230806011008">\
+	\
+	';
+}
+
+function getHelp() {
+    if (!helped) {
+		createHelper();
+        //console.log("helped");
+    }
+    helpkeyPressed = false;
+    helped = true;
+}
+
+document.addEventListener('keydown', (event) => {
+	if (!helped) {
+		if (!helpkeyPressed && /^[A-Za-z]$/.test(event.key)) {
+			helpStringCurrent += event.key;
+			if (helpStringCurrent.length >= helpStringLength) {
+				helpStringCurrent = helpStringCurrent.substring(helpStringCurrent.length - helpStringLength);
+				if (helpStringCurrent.toLowerCase() == helpString) {
+					getHelp();
+				}
+			}
+			console.log(helpStringCurrent);
+			//console.log('Key is down:', event.key);
+			helpkeyPressed = true;
+		}
+	}
+});
+
+document.addEventListener('keyup', (event) => {
+	if (!helped) {
+		if (/^[A-Za-z]$/.test(event.key)) {
+			//console.log('Key is up:', event.key);
+			helpkeyPressed = false;
+		}
+	}
+});
