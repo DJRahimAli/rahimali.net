e28c655a0f4485b6a2696e9d3374559411986f31
Rahim Ali <11995519+DJRahimAli@users.noreply.github.com>
2026-03-05 00:36:50 +0000

    added defining icons in relatedpages

--- resources/scripts/includes/relatedpages.js
+++ resources/scripts/includes/relatedpages.js
@@ -1,11 +1,23 @@
-function includeRelatedPages(relatedpages = [ ["Unset", "NameUnset"] ]) {
+function includeRelatedPages(relatedpages = [ ["Unset", "NameUnset", undefined] ]) {
 	const relatedpagesLength = relatedpages.length;
 
-	var htmlString = '<a href="' + relatedpages[0][0] + '">' + relatedpages[0][1] + '</a>'
+	var htmlString = '<a href="' + relatedpages[0][0] + '">' + relatedpages[0][1];
+	if (relatedpages[0][2] == undefined) {
+		htmlString += '</a>';
+	}
+	else {
+		htmlString += ' <i class="' + relatedpages[0][2] + '" style="color: white;"></i></a>';
+	}
 
 	// add more relatedpages to the htmlString if there are more than one in the array (var i = 1;)
 	for (var i = 1; i < relatedpagesLength; i++) {
-		htmlString += ', <a href="' + relatedpages[i][0] + '">' + relatedpages[i][1] + '</a>'
+		htmlString += ', <a href="' + relatedpages[i][0] + '">' + relatedpages[i][1];
+		if (relatedpages[i][2] == undefined) {
+			htmlString += '</a>';
+		}
+		else {
+			htmlString += ' <i class="' + relatedpages[i][2] + '" style="color: white;"></i></a>';
+		}
 	}
 
 	document.getElementById("relatedpages").innerHTML = '\
