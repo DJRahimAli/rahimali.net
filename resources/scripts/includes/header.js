7ba9a1471a8d4da7c063cc9c3c32e193e8c1d398
Rahim Ali <11995519+DJRahimAli@users.noreply.github.com>
2026-08-02 23:47:51 +0100

    add "ninButton" easter egg

--- resources/scripts/includes/header.js
+++ resources/scripts/includes/header.js
@@ -11,7 +11,7 @@ function includeHeader() {
 				Gallery <i class="fa fa-angle-down"></i>\
 			</button>\
 			<div class="navbarDropdown-Content">\
-				<a href="/gallery">Everything</a>\
+				<a id="ninButton" href="/gallery">Everything</a>\
 			<hr class="nav">\
 				<a href="/gallery/art">Art</a>\
 			<hr class="nav">\
@@ -39,3 +39,53 @@ function includeHeader() {
 }
 
 includeHeader();
+
+let btn = document.getElementById("ninButton");
+let btnHover = false;
+const btnNotes = [
+	[-400, 3],
+	[-600, 2],
+	[-700, 2],
+	[-900, 2],
+	[-1100, 3],
+	[-900, 3],
+	[-1100, 2],
+	[-1200, 2],
+	[-1400, 2],
+	[-1600, 2],//
+	[-400, 3],
+	[-600, 2],
+	[-700, 2],
+	[-900, 2],
+	[-1100, 2],
+	[-1200, 1],
+	[-1100, 1],
+	[-1200, 2],
+	[-1200, 11]//
+];
+let btnNotesCurrent = [0, btnNotes[0][1]];
+//console.log(btnNotesCurrent);
+loadSound("button");
+
+function progressNotesCurrent() {
+	//console.log(btnNotesCurrent);
+	btnNotesCurrent[1]--;
+	if (btnNotesCurrent[1] == 0) {
+		btnNotesCurrent[0]++;
+		btnNotesCurrent[0] = btnNotesCurrent[0] % (btnNotes.length);
+		btnNotesCurrent[1] = btnNotes[ btnNotesCurrent[0] ][1];
+	}
+	//console.log(btnNotesCurrent);
+}
+
+btn.addEventListener("mouseover", function() {
+    if (btnHover) return;
+	if (!keys) return;
+	playSound("button", btnNotes[ btnNotesCurrent[0] ][0]);
+	progressNotesCurrent()
+	btnHover = true;
+});
+
+btn.addEventListener("mouseout", function() {
+	btnHover = false;
+});
